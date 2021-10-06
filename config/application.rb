719f8107c5ffd7653bb692ccd80c7c4e69916cde
Agis Anastasopoulos <agis@clerk.dev>
2021-10-06 11:31:06 +0300

    fixes

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module ClerkRailsStarter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.middleware.delete Clerk::RackMiddleware
-    config.middleware.use Clerk::RackMiddlewareV2
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
